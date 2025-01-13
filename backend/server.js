af102eec93a23973187c7581e3597e705112bad1
Yusuf <safarov.1513@gmail.com>
2025-01-14 03:06:51 +0500

    test

--- backend/server.js
+++ backend/server.js
@@ -68,7 +68,7 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../frontend/dist', 'index.html'));
 });
 
-// Start the server
+// Start the server.
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
